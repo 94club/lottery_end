ebacaf3e393d7cda121653835ad0c4d7e1feaeab
alistar <alistar@e-veb.com>
2018-12-21 15:25:56 +0800

    update

--- controller/user.js
+++ controller/user.js
@@ -27,36 +27,7 @@ class User extends BaseComponent{
     this.commitComment = this.commitComment.bind(this)
   }
 
-  /**
-   *
-   * @api {post} /api/login  登录
-   * @apiName 登录
-   * @apiGroup admin
-   * @apiVersion 1.0.0
-   * @apiDescription 用户登录
-   *
-   * @apiParam {String} username 用户名
-   *
-   * @apiSuccess {String} status 结果码
-   * @apiSuccess {String} message 消息说明
-   * 
-   * @apiSuccessExample {json}Success-Response:
-   *  HTTP/1.1 200 OK
-   * {
-   *   status: 200,
-   *   message: '登录/注册成功',
-   *   data: {
-   *     token: token
-   *   }
-   * }
-   *
-   *  @apiErrorExample {json} Error-Response:
-   *  HTTP/1.1 200
-   *  {
-   *   status: 0,
-   *   message: '失败原因',
-   *  }
-   */
+
   async login(req, res, next) {
     let role = 0 // 0代表普通用户 1代表管理员
     let username = req.body.username
@@ -141,32 +112,7 @@ class User extends BaseComponent{
     }
   }
 
-  /**
-   *
-   * @api {post} /user/getUserInfo  用户信息
-   * @apiName 用户信息
-   * @apiGroup admin
-   * @apiVersion 1.0.0
-   * @apiDescription 获取用户信息
-   *
-   * @apiSuccess {String} status 结果码
-   * @apiSuccess {String} message 消息说明
-   * 
-   * @apiSuccessExample {json}Success-Response:
-   *  HTTP/1.1 200 OK
-   * {
-   *   status: 200,
-   *   message: 'success',
-   *   data: {"avatar":"/public/img/avatar.jpg","hadPrize":false,"username":"alistar-wang","role":0,"createTime":"2018/11/10 14:24:25","id":1}}
-   * }
-   *
-   *  @apiErrorExample {json} Error-Response:
-   *  HTTP/1.1 200
-   *  {
-   *   status: 0,
-   *   message: '查询失败',
-   *  }
-   */
+
   async getUserInfo(req, res, next) {
     let userInfo = await UserModel.findOne({
       username: req.user.username
@@ -188,31 +134,7 @@ class User extends BaseComponent{
     }
   }
 
-  /**
-   *
-   * @api {post} /user/logout  用户登出
-   * @apiName 用户登出
-   * @apiGroup admin
-   * @apiVersion 1.0.0
-   * @apiDescription 用户登出
-   *
-   * @apiSuccess {String} status 结果码
-   * @apiSuccess {String} message 消息说明
-   * 
-   * @apiSuccessExample {json}Success-Response:
-   *  HTTP/1.1 200 OK
-   * {
-   *   status: 200,
-   *   message: 'success'
-   * }
-   *
-   *  @apiErrorExample {json} Error-Response:
-   *  HTTP/1.1 200
-   *  {
-   *   status: 0,
-   *   message: '登出失败',
-   *  }
-   */
+
   async logout(req, res, next) {
     // 清除redis中的token
     res.json({
@@ -222,31 +144,7 @@ class User extends BaseComponent{
     redisManager.remove(req)
   }
 
-  /**
-   *
-   * @api {get} /user/remainTime  离晚会开始剩余时间
-   * @apiName 离晚会开始剩余时间
-   * @apiGroup admin
-   * @apiVersion 1.0.0
-   * @apiDescription 离晚会开始剩余时间
-   *
-   * @apiSuccess {String} status 结果码
-   * @apiSuccess {String} message 消息说明
-   * 
-   * @apiSuccessExample {json}Success-Response:
-   *  HTTP/1.1 200 OK
-   * {
-   *   status: 200,
-   *   message: '查询成功'
-   * }
-   *
-   *  @apiErrorExample {json} Error-Response:
-   *  HTTP/1.1 200
-   *  {
-   *   status: 0,
-   *   message: '查询失败',
-   *  }
-   */
+ 
   async remainTime(req, res) {
     let currentTime = new Date().getTime()
     let prepareTime = new Date('2019', '0', '17', '18').getTime()
@@ -285,31 +183,7 @@ class User extends BaseComponent{
     }
   }
 
-  /**
-   *
-   * @api {post} /user/updateUserInfo  更新用户信息
-   * @apiName 更新用户信息
-   * @apiGroup admin
-   * @apiVersion 1.0.0
-   * @apiDescription 更新用户信息
-   *
-   * @apiSuccess {String} status 结果码
-   * @apiSuccess {String} message 消息说明
-   * 
-   * @apiSuccessExample {json}Success-Response:
-   *  HTTP/1.1 200 OK
-   * {
-   *   status: 200,
-   *   message: '更新成功'
-   * }
-   *
-   *  @apiErrorExample {json} Error-Response:
-   *  HTTP/1.1 200
-   *  {
-   *   status: 0,
-   *   message: '更新失败',
-   *  }
-   */
+
   async updateUserInfo (req, res, next) {
     const {hadPrize, prizeName} = req.body
     try {
@@ -338,34 +212,7 @@ class User extends BaseComponent{
     }
   }
 
-  /**
-   *
-   * @api {post} /user/uploadAvatar  更新用户图像
-   * @apiName 更新用户图像
-   * @apiGroup admin
-   * @apiVersion 1.0.0
-   * @apiDescription 更新用户图像
-   *
-   * @apiSuccess {String} status 结果码
-   * @apiSuccess {String} message 消息说明
-   * 
-   * @apiSuccessExample {json}Success-Response:
-   *  HTTP/1.1 200 OK
-   * {
-   *   status: 200,
-   *   message: '更新成功',
-   *   data: {
-   *     imgPath: '/public/img/avatar.jpg'
-   *   }
-   * }
-   *
-   *  @apiErrorExample {json} Error-Response:
-   *  HTTP/1.1 200
-   *  {
-   *   status: 0,
-   *   message: '更新失败',
-   *  }
-   */
+
   async uploadAvatar (req, res, next) {
     const form = new formidable.IncomingForm()
     let username = req.user.username
@@ -410,32 +257,7 @@ class User extends BaseComponent{
     })
   }
 
-  /**
-   *
-   * @api {get} /user/getAllUser  获取所有用户
-   * @apiName 获取所有用户
-   * @apiGroup admin
-   * @apiVersion 1.0.0
-   * @apiDescription 获取所有用户
-   *
-   * @apiSuccess {String} status 结果码
-   * @apiSuccess {String} message 消息说明
-   * 
-   * @apiSuccessExample {json}Success-Response:
-   *  HTTP/1.1 200 OK
-   * {
-   *   status: 200,
-   *   message: '更新成功',
-   *   data: [{"avatar":"/public/img/167c5b72dcb.jpg","hadPrize":false,"_id":"5c18a2a5bb5f3d1f0c994a21","username":"alistar-wang","lang":"cn","role":0,"createTime":"2018/12/18 15:32:53","id":1,"__v":0},{"avatar":"/public/img/avatar.jpg","hadPrize":false,"_id":"5c1a0c551527ab39f96293b0","username":"123eveb-admin456","lang":"cn","role":1,"createTime":"2018/12/19 17:16:05","id":2,"__v":0}]
-   * }
-   *
-   *  @apiErrorExample {json} Error-Response:
-   *  HTTP/1.1 200
-   *  {
-   *   status: 0,
-   *   message: '更新失败',
-   *  }
-   */
+
   async getAllUser (req, res, next) {
     let info = await UserModel.find({})
     if (info) {
@@ -452,31 +274,7 @@ class User extends BaseComponent{
     }
   }
 
-  /**
-   *
-   * @api {post} /user/uploadFashionImg  根据id更新服装图片
-   * @apiName 根据id更新服装图片
-   * @apiGroup admin
-   * @apiVersion 1.0.0
-   * @apiDescription 根据id更新服装图片
-   *
-   * @apiSuccess {String} status 结果码
-   * @apiSuccess {String} message 消息说明
-   * 
-   * @apiSuccessExample {json}Success-Response:
-   *  HTTP/1.1 200 OK
-   * {
-   *   status: 200,
-   *   message: '更新成功',
-   * }
-   *
-   *  @apiErrorExample {json} Error-Response:
-   *  HTTP/1.1 200
-   *  {
-   *   status: 0,
-   *   message: '更新失败',
-   *  }
-   */
+
   async uploadFashionImg (req, res, next) {
     const form = new formidable.IncomingForm()
     form.parse(req, async (err, fields, files) => {
@@ -521,31 +319,7 @@ class User extends BaseComponent{
     })
   }
 
-  /**
-   *
-   * @api {post} /user/fashionImgAdd  添加最佳服装
-   * @apiName 添加最佳服装
-   * @apiGroup admin
-   * @apiVersion 1.0.0
-   * @apiDescription 添加最佳服装
-   *
-   * @apiSuccess {String} status 结果码
-   * @apiSuccess {String} message 消息说明
-   * 
-   * @apiSuccessExample {json}Success-Response:
-   *  HTTP/1.1 200 OK
-   * {
-   *   status: 200,
-   *   message: '添加成功',
-   * }
-   *
-   *  @apiErrorExample {json} Error-Response:
-   *  HTTP/1.1 200
-   *  {
-   *   status: 0,
-   *   message: '添加失败原因',
-   *  }
-   */
+
   async fashionImgAdd (req, res, next) {
     let id = req.body.id
     try {
@@ -585,32 +359,7 @@ class User extends BaseComponent{
 
   }
 
-  /**
-   *
-   * @api {get} /user/getFashionImgList  获取所有最佳服装
-   * @apiName 获取所有最佳服装
-   * @apiGroup admin
-   * @apiVersion 1.0.0
-   * @apiDescription 获取所有最佳服装
-   *
-   * @apiSuccess {String} status 结果码
-   * @apiSuccess {String} message 消息说明
-   * 
-   * @apiSuccessExample {json} Success-Response:
-   *  HTTP/1.1 200 OK
-   * {
-   *   status: 200,
-   *   message: '获取成功',
-   *   data: []
-   * }
-   *
-   *  @apiErrorExample {json} Error-Response:
-   *  HTTP/1.1 200
-   *  {
-   *   status: 0,
-   *   message: '获取失败原因',
-   *  }
-   */  
+  
   async getFashionImgList (req, res, next) {
     let info = await FashionModel.find({}, {'_id': 0, '__v': 0})
     if (info) {
